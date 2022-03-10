49dbd48c2faaa570327225fd8de162f6b598913d
HirasParasian <hirasparasian@gmail.com>
2022-03-10 14:30:06 +0700

    Feat : Login Auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 
-import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Test from './pages/Test'
 import Login from './pages/Login'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 export const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const token = window.localStorage.getItem('token')
+    if (token) {
+      dispatch({
+        type: 'AUTH_LOGIN_FULFILLED',
+        payload: {
+          data: {
+            results: { token }
+          }
+        }
+      })
+    }
+  }, [])
   return (
     <BrowserRouter>
       <Routes>
