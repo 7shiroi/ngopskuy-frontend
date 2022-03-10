b346c68ed43a9e2dbf59505c3d2e522a1a05568e
HirasParasian <hirasparasian@gmail.com>
2022-03-10 11:32:17 +0700

    Update

---
+++ src/pages/Login.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Login = () => {
+    return (
+        <>
+            <div className='container-fluid'>
+                <div className='row'>
+                    {/* Left */}
+                    <div className='col-6'>
+                        
+                    </div>
+                    {/* Right */}
+                    <div className='col-6'>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login
