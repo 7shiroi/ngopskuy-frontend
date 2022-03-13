ebc454d8d6c42ed4b77152d4a504cd6d216976c7
HirasParasian <hirasparasian@gmail.com>
2022-03-13 16:01:20 +0700

    Update : Navbar

--- src/components/NavbarHome.js
+++ src/components/NavbarHome.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-import-assign */
 /* eslint-disable no-redeclare */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Button from './Button'
 import { useNavigate } from 'react-router-dom'
@@ -8,9 +8,12 @@ import { useDispatch, useSelector } from 'react-redux'
 import Logo from '../assets/images/Logo.png'
 import Navbar from 'react-bootstrap/Navbar'
 import Profile from '../assets/images/photo.png'
+import { getProfile } from '../redux/actions/auth'
 
 export const NavbarHome = () => {
 	const auth = useSelector(state => state.auth)
+	const token = useSelector(state => state.auth.token)
+	console.log(auth.userData)
 	const dispatch = useDispatch()
 	const navigate = useNavigate()
 	const goLogin = () => {
@@ -19,6 +22,10 @@ export const NavbarHome = () => {
 	const goSignUp = () => {
 		navigate(`/signup`)
 	}
+	useEffect(() => {
+		const token = window.localStorage.getItem('token')
+		dispatch(getProfile(token))
+	}, [])
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-pallet-4">
 			<div className="container">
@@ -36,24 +43,24 @@ export const NavbarHome = () => {
 				</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-						<li className="nav-item mx-3">
+						<li className="nav-item my-auto mx-auto ">
 							<Link className='nav-link rb fw-bold text-pallet-1 fs-5' aria-currrent='page' to='/'>Home</Link>
 						</li>
-						<li className="nav-item mx-3">
+						<li className="nav-item my-auto mx-auto">
 							<Link className='nav-link rb fs-5' aria-currrent='page' to='/product'>Product</Link>
 						</li>
-						<li className="nav-item mx-3">
+						<li className="nav-item my-auto mx-auto">
 							<Link className='nav-link rb fs-5' aria-currrent='page' to='/cart'>Yourt Cart</Link>
 						</li>
-						<li className="nav-item mx-3">
+						<li className="nav-item my-auto mx-auto">
 							<Link className='nav-link rb fs-5' aria-currrent='page' to='/history'>History</Link>
 						</li>
 						{auth.token &&
 							<li className="nav-item dropdown mx-auto">
 								<div className="nav-link dropdown-toggle img-profile-login" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
 									aria-expanded="false">
-									<Link className="" to="/profil">
-										<img src={Profile} width="50"
+									<Link className="" to="/profile">
+										<img src={auth.userData.image || Profile} width="50"
 											height="50" alt="Avatar Logo" className=" rounded-pill" />
 									</Link>
 								</div>
