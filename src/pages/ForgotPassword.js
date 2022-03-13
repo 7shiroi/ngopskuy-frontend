c4252884fc3b9cde00df9142233a88e1a871577d
HirasParasian <hirasparasian@gmail.com>
2022-03-13 08:56:03 +0700

    Feat : Reset Password

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -14,11 +14,15 @@ import { useDispatch, useSelector } from 'react-redux'
 export const ForgotPassword = () => {
 	const auth = useSelector(state => state.auth)
 	const dispatch = useDispatch()
+	console.log(auth.email)
 
 	const onRequestOTP = (e) => {
 		e.preventDefault()
 		const email = e.target.elements['email'].value
-		dispatch(requestResetPassword(email))
+		const code = e.target.elements['code'].value
+		const password = e.target.elements['password'].value
+		const confirm_password = e.target.elements['confirm_password'].value
+		dispatch(requestResetPassword(email, code, password, confirm_password))
 		dispatch({
 			type: 'SET_EMAIL',
 			payload: { email }
@@ -31,38 +35,50 @@ export const ForgotPassword = () => {
 				<Card.ImgOverlay className='d-flex flex-column container'>
 					<div className='row'>
 						<div className='col-12 text-center'>
-							<div className='my-3'>
+							<div className=''>
 								<h1 className='d-none d-md-block mt-5 pt-5'><b>Forgot your password?</b></h1>
 								<h1 className='d-md-none text-dark'>Don’t worry</h1>
 							</div>
 							<img className='img-fluid d-md-none ' src={BackgroundMobile} alt="bg-mobile"></img>
-							<Card.Text as="h5" className='mb-5 pb-5'>
-                                Don’t worry, we got your back!
+							<Card.Text as="h5" className=''>
+								Don’t worry, we got your back!
 							</Card.Text>
 
 						</div>
 						{
 							auth.errorMsg &&
-                            <div className="alert alert-warning fade show" role="alert">
-                            	<strong>{auth.errorMsg[0]}</strong>
-                            </div>
+							<div className="alert alert-warning fade show" role="alert">
+								<strong>{auth.errorMsg}</strong>
+							</div>
 						}
 						{
 							auth.errMsg &&
-                            <div className="alert alert-warning fade show" role="alert">
-                            	<strong>{auth.errMsg}</strong>
-                            </div>
+							<div className="alert alert-warning fade show" role="alert">
+								<strong>{auth.errMsg}</strong>
+							</div>
 						}
 						{
 							auth.successMsg &&
-                            <div className="alert alert-success fade show" role="alert">
-                            	<strong>{auth.successMsg}</strong>
-                            </div>
+							<div className="alert alert-success fade show" role="alert">
+								<strong>{auth.successMsg}</strong>
+							</div>
 						}
 						<form onSubmit={(e) => onRequestOTP(e)}>
 							<div className='col-12 d-none d-md-block'>
-								<Input name="email" version="background-transparent py-4 bg-pallet-4 outline-0 border-0"
+								<Input name="email" defaultValue={auth.email} version="background-transparent py-4 bg-pallet-4 outline-0 border-0"
 									block placeholder="Enter your email adress to get link"></Input>
+							</div>
+							<div className='col-12 d-none d-md-block'>
+								<Input name="code" version="background-transparent py-4 bg-pallet-4 outline-0 border-0"
+									block placeholder="Enter Code"></Input>
+							</div>
+							<div className='col-12 d-none d-md-block'>
+								<Input name="password" version="background-transparent py-4 bg-pallet-4 outline-0 border-0"
+									block placeholder="Enter new password"></Input>
+							</div>
+							<div className='col-12 d-none d-md-block'>
+								<Input name="confirm_password" version="background-transparent py-4 bg-pallet-4 outline-0 border-0"
+									block placeholder="Enter new password again!"></Input>
 							</div>
 							<div className='col-12 d-md-none'>
 								<InputUnderline version="input-underline background-transparent py-4 "
@@ -79,7 +95,7 @@ export const ForgotPassword = () => {
 								<b>Click Again if you didn’t receive any link in 2 minutes</b>
 							</Card.Text>
 							<Card.Text className='d-md-none'>
-                                Haven’t received any link?
+								Haven’t received any link?
 							</Card.Text>
 							{/* <div className='col-12 pt-4 d-flex justify-content-center'>
                                 <div className='col-12 col-lg-6 col-md-6 col-xl-4'>
