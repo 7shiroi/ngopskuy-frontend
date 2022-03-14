e08ea8e159248c99f8dcf8c9e4ae9ead59c428be
HirasParasian <hirasparasian@gmail.com>
2022-03-14 10:32:58 +0700

    Update : Navbar

--- src/components/NavbarHome.js
+++ src/components/NavbarHome.js
@@ -1,7 +1,13 @@
+
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Button from './Button'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import Logo from '../assets/images/Logo.png'
+import Navbar from 'react-bootstrap/Navbar'
+import Profile from '../assets/images/photo.png'
+import { getProfile } from '../redux/actions/auth'
 
 export const NavbarHome = () => {
 	const auth = useSelector(state => state.auth)
@@ -15,20 +21,29 @@ export const NavbarHome = () => {
 	const goSignUp = () => {
 		navigate(`/signup`)
 	}
+
 	useEffect(() => {
 		const token = window.localStorage.getItem('token')
 		dispatch(getProfile(token))
 	}, [])
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-pallet-4">
-			<div className="container py-4">
-				<div className="navbar-brand rb fw-bold text-pallet-1" href="#">NgopSkuY</div>
+			<div className="container">
+				<Navbar.Brand href="/">
+					<img
+						src={Logo}
+						width="120"
+						height="120"
+						className="d-inline-block align-top"
+						alt="React Bootstrap logo"
+					/>
+				</Navbar.Brand>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-						{auth.userData.id_role === 3 &&
+						{auth.userData.id_role === 3 || tokens === null &&
 							<li className="nav-item my-auto mx-auto ">
 								<Link className='nav-link rb fw-bold text-pallet-1 fs-5' aria-currrent='page' to='/'>Home</Link>
 							</li>
@@ -38,7 +53,7 @@ export const NavbarHome = () => {
 								<Link className='nav-link rb fw-bold text-pallet-1 fs-5' aria-currrent='page' to='/dashboard'>Home</Link>
 							</li>
 						}
-						{auth.userData.id_role === 3 &&
+						{auth.userData.id_role === 3 || tokens === null &&
 							<li className="nav-item my-auto mx-auto">
 								<Link className='nav-link rb fs-5' aria-currrent='page' to='/product'>Product</Link>
 							</li>
@@ -105,17 +120,6 @@ export const NavbarHome = () => {
 							</li>
 						}
 					</ul>
-					<div className="d-flex">
-						<div className=' d-flex align-items-center'>
-							<Button onClick={goLogin} block variant='pallet-4 px-5 py-2 fs-5 rb text-pallet-1 fw-bold' padding='mx-1' >Login</Button>
-						</div>
-						<div className=' d-flex align-items-center'>
-							<Button onClick={goSignUp} block variant='pallet-1 px-5 py-2 fs-5 rb' padding='mx-1' >Sign Up</Button>
-						</div>
-						{/* <div className='w-100'>
-							<Button block variant='pallet-1 px-5 py-2 '>Sign Up</Button>
-						</div> */}
-					</div>
 				</div>
 			</div >
 		</nav >
