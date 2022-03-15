fd24f5bd550c4a74b5e9bccd5e936f5bfb9adbe6
HirasParasian <hirasparasian@gmail.com>
2022-03-15 08:09:38 +0700

    Update : History

--- src/components/ModalDelete.js
+++ src/components/ModalDelete.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 import trashIcon from '../assets/images/trash-icon.png'
 import { deleteHistory, getHistory } from '../redux/actions/history';
 
-export const ModalDelete = ({id, img, product, price, status, toDelete}) => {
+export const ModalDelete = ({ id, img, product, price, status, toDelete }) => {
     const [isOpen, setIsOpen] = useState(false);
     const token = window.localStorage.getItem('token')
     const dispatch = useDispatch()
@@ -18,26 +18,31 @@ export const ModalDelete = ({id, img, product, price, status, toDelete}) => {
         dispatch(getHistory(token))
     }
     return (
-        <div className='block position-relative radius bg-white' style={{height: '100%'}}>
+        <div className='block position-relative radius bg-white' style={{ height: '100%' }}>
             <div className='d-flex align-items-center p-3'>
                 <div className='img-fit'>
-                    <img src={img} alt='corndog' width='50' height='50' className='rounded-circle'/>
+                    <img src={img} alt='corndog' width='50' height='50' className='rounded-circle' />
                 </div>
-                <div className="about px-3 text-pallet-1" style={{fontSize: '15px'}}>
+                <div className="about px-3 text-pallet-1" style={{ fontSize: '15px' }}>
                     <p className='fs-6 fw-bold py-0 my-0'>{product}</p>
                     <p className='py-0 my-0'>IDR {price}</p>
                     <p className='py-0 my-0'>{status}</p>
                 </div>
             </div>
             {!isOpen &&
-            <div className='clickDelete bg-transparent position-absolute top-0 start-0' onClick={onShow}></div>}
+                <div className='clickDelete bg-transparent position-absolute top-0 start-0' onClick={onShow}></div>}
             {isOpen &&
-            <div className='clickDelete bg-dark bg-opacity-10 position-absolute top-0 start-0' onClick={onShow}></div>}
+                <div className='clickDelete bg-dark bg-opacity-10 position-absolute top-0 start-0' onClick={onShow}></div>}
             {isOpen && <div className='position-absolute top-0 end-0 translate-middle-y d-flex'>
                 <div className="icon-trash bg-pallet-1 text-center me-3">
-                <div onClick={onDelete(id)}>
-                    <img src={trashIcon} alt='delete-icon'/>
-                </div></div>
+                    <div>
+                        <img src={trashIcon} alt='delete-icon'
+                            onClick={() => {
+                                dispatch(deleteHistory(id, token));
+                                dispatch(getHistory(token))
+                            }} />
+                    </div>
+                </div>
                 <div className='icon-close text-pallet-1 text-center bg-warning p-0 m-0' onClick={onHidden}>X</div>
             </div>
             }
--- src/redux/reducers/history.js
+++ src/redux/reducers/history.js
@@ -7,51 +7,51 @@ const initialState = {
     errorMsg: ''
 }
 
-const history = (state=initialState, action) => {
-    switch(action.type) {
+const history = (state = initialState, action) => {
+    switch (action.type) {
         case 'GET_HISTORY_PENDING': {
             state.isLoading = true
             state.isError = false
-            return state
+            return { ...state }
         }
         case 'GET_HISTORY_FULFILLED': {
-            const {data} = action.payload
+            const { data } = action.payload
             state.isLoading = false
             state.isError = false
             state.history = data.result
             window.localStorage.setItem('userHistory', JSON.stringify(state.userHistory))
-            return state
+            return { ...state }
         }
         case 'GET_HISTORY_REJECTED': {
-          const {error} = action.payload.response.data
-          console.log(error)
-          state.isLoading = false
-          state.isError = true
-          state.errorMsg = error
-          return state
+            const { error } = action.payload.response.data
+            console.log(error)
+            state.isLoading = false
+            state.isError = true
+            state.errorMsg = error
+            return { ...state }
         }
         case 'DELETE_HISTORY_PENDING': {
             state.isLoading = true
             state.isError = false
-            return state
+            return { ...state }
         }
         case 'DELETE_HISTORY_FULFILLED': {
-            const {data} = action.payload
+            const { data } = action.payload
             state.isLoading = false
             state.isError = false
             state.message = data.message
-            return state
+            return { ...state }
         }
         case 'DELETE_HISTORY_REJECTED': {
-          const {error} = action.payload.response.data
-          console.log(error)
-          state.isLoading = false
-          state.isError = true
-          state.errorMsg = error
-          return state
+            const { error } = action.payload.response.data
+            console.log(error)
+            state.isLoading = false
+            state.isError = true
+            state.errorMsg = error
+            return { ...state }
         }
         default: {
-            return {...state}
+            return { ...state }
         }
     }
 }
